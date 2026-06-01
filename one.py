64954483f14ede5855fc7314d054f97aaaba13fc
akhshyganesh <akhshyganeshb@gmail.com>
2026-06-01 22:05:24 +0530

    feat: add initial project structure with main files and requirements

---
+++ one.py
@@ -0,0 +1,8 @@
+from two import call_me_two
+
+
+def call_me_one():
+    print("Hello from one!")
+
+call_me_one()
+call_me_two()
---
+++ two.py
@@ -0,0 +1,6 @@
+def call_me_two():
+    print("Hello from two!")
+
+if __name__ == "__main__":
+    call_me_two()
+
